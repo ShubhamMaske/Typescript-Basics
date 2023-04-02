373980bdb73045973158516703875fd12b92160d
ShubhamMaske <shubhammaske099@gmail.com>
2023-04-02 12:53:27 +0530

    commit on integrating typescript & node

--- app.ts
+++ app.ts
@@ -1,55 +1,12 @@
+import  express from 'express';
+import bodyParser from 'body-parser';
 
-var num1Element = document.getElementById('num1') as HTMLInputElement;
-var num2Element = document.getElementById('num2') as HTMLInputElement;
-var buttonElement = document.querySelector('button')!;
+import todosRoute from './routes/todos';
 
-type numOrString = number | string;                     // alias using type operator
-type result = { val: number; timestamp: Date}
+const app = express();
 
-interface resultObject {
-    val: number;
-    timestamp: Date;
-}
+app.use(bodyParser.json());
 
-function add(num1: numOrString, num2:numOrString){    //union type = | 
-    if(typeof num1 === 'number' && typeof num2 === 'number'){
-        return num1 + num2;
-    }else if(typeof num1 === 'string' && typeof num2 === 'string'){
-        return num1 + ' ' + num2
-    }
-    return +num1 + +num2;
-}
+app.use(todosRoute)
 
-const numResult: number[] = [];
-const textResult: string[] = [];
-
-function printResult(resultObj: result){
-    console.log(resultObj.val);
-}
-
-buttonElement.addEventListener('click', addition)
-
-function addition(){
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const result = add(+num1, +num2);
-    numResult.push(result as number);
-
-    const stringResult = add(num1 , num2)
-    textResult.push(stringResult as string);
-    // console.log(result);
-    // console.log(sttringResult);
-
-    printResult({val: result as number, timestamp: new Date()});
-    console.log(numResult, textResult)
-}
-
-const myPromise = new Promise<string>((resolve, reject) => {
-    setTimeout(()=>{
-        resolve("It worked!");
-    },1000)
-})
-
-myPromise.then(result => {
-    console.log(result.split('w'));
-})
+app.listen(3000);
---
+++ routes/todos.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+
+import {Todo} from '../models/todo'
+
+let todos: Todo[] = [];
+
+const router = Router();
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({todos : todos})
+});
+
+router.post('/todo',(req, res, next) => {
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    }
+
+    todos.push(newTodo);
+    res.status(200).json({success:true});
+})
+
+router.post('/todo/delete/:todoId',(req, res, next) => {
+    todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    
+        return res.status(200).json({message: 'todo Item Deleted'});
+
+})
+
+router.post('/todo/edit/:todoId',(req, res, next) => {
+    const todoId = req.params.todoId;
+    const todoIndex = todos.findIndex( todoItem => todoItem.id === todoId);
+    if(todoIndex >= 0){
+        todos[todoIndex] = {
+            id: todos[todoIndex].id,
+            text: req.body.text
+        }
+        return res.status(200).json({message:'todo Item updated',todos: todos})
+    }
+    res.status(404).json({message: "Item not found"})
+})
+
+export default router;
